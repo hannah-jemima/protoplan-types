51a9415c51bc9e4519261a4549a6e9b834ad077e
Hannah <hannah144@protonmail.com>
2022-08-09 18:43:47 +0100

    add shared types

---
+++ src/country.ts
@@ -0,0 +1,12 @@
+
+
+export interface TCountry
+{
+  countryId: number,
+  name: string,
+  code: string,
+  currencyCode: string
+}
+
+export type TCountries = TCountry[];
+
---
+++ src/protocol.ts
@@ -0,0 +1,35 @@
+import { TListingCosts } from "./listing.js";
+
+
+export interface TProtocolRow extends TProtocolRowCosts
+{
+  productName: string,
+  listingName: string,
+  brandName: string,
+  amountUnit: string,
+  recDoseUnitId: number,
+  formId: number,
+  listingCurrencyCode: string,
+  priority: number,
+  vendorId: number,
+  vendorName: string,
+  scrapeTime: Date,
+}
+
+export type TProtocol = TProtocolRow[]
+
+export interface TProtocolRowCosts extends TListingCosts
+{
+  protocolId: number,
+  dose: number,
+  doseUnitId: number,
+  amount: number,
+  amountUnitId: number,
+  dosesPerDay: number,
+  daysPerMonth: number,
+  productsPerMonth: number,
+  listingsPerMonth: number,
+  repurchase: number,
+  costPerMonth: number,
+  feesPerMonth: number
+}
