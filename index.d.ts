72885a97c498cfe42279ecfd548f9b0953c27b62
Hannah <hannah144@protonmail.com>
2023-02-21 19:48:39 +0000

    publishing

--- index.d.ts
+++ index.d.ts
@@ -185,6 +185,7 @@ export interface IListingInfo extends TProduct, IListingCostCalculationData
   scrapeTime: Date | null;
   vendorId: number;
   vendorName: string;
+  vendorInfo: string | null;
   inaccessible: boolean;
   currencyId: number;
   url: string;
